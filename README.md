e2b7fd3833d98393a5a59465541a71254d0c427c
yasikrehman <yasikrehman007@gmail.com>
2022-04-13 16:10:46 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-3qv6ds
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-3qv6ds)
---
+++ index.js
@@ -0,0 +1,15 @@
+function getIndex(...args) {
+  let [sumArray, target] = args;
+  const arr = [];
+  let getAr = sumArray.filter((nums, i, self) => {
+    let final = target - nums;
+    if (self.indexOf(final) !== -1) {
+      arr.push(self.indexOf(final));
+    }
+  });
+  return arr;
+}
+
+let sum = getIndex([1, 16, 5, 12, 3], 8);
+
+console.log(sum);
